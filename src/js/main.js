54b97df140e68fe8a0755079a98da1d0225c70bb
shiinumint <sandr45tephany@gmail.com>
2025-07-24 14:57:44 +0700

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Import main SCSS file (which includes Bootstrap customization)
+import '../scss/styles.scss';
+
+// Import Bootstrap JS
+import * as bootstrap from 'bootstrap';
+
+// Optionally import specific components
+// import { Tooltip, Toast, Popover } from 'bootstrap';
